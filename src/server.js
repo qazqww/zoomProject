7fb1defe38c12da1a435dc7c3280b2ff2c1749dc
이진곤 <qazqww@naver.com>
2023-04-21 17:02:32 +0900

    Section 1 Complete

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from 'ws';
 import express from "express";
 
 const app = express();
@@ -5,8 +7,37 @@ const app = express();
 app.set("view engine", "pug")
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+// http서버 위에 ws서버를 만들어서 동일한 포트에서 http, wss 모두 처리
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+// wss는 서버를 위한 것, socket은 각 브라우저에 해당
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anonymous";
+    console.log("Connected to Browser ✅");
+    socket.on("close", () => console.log("Disconnected from Browser ❌"));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) =>
+                    aSocket.send(`${socket.nickname}: ${message.payload}`)
+                );
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break;
+        }
+    });
+})
+
+server.listen(3000, handleListen);
